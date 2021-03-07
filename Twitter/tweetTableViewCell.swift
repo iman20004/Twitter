3ea9e9dedfa253bcff1591fb5861c92b9f18fbdb
iman20004 <imanali4@gmail.com>
2021-03-07 14:29:26 -0500

    Retweet

--- Twitter/tweetTableViewCell.swift
+++ Twitter/tweetTableViewCell.swift
@@ -37,8 +37,24 @@ class tweetTableViewCell: UITableViewCell {
     }
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Retweeting did not succedd \(error)")
+        })
     }
     
+    
+    func setRetweeted(_ isRetweeted: Bool){
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+        
+    }
     
     func setFav(_ isFavourited: Bool){
         favorited = isFavourited
